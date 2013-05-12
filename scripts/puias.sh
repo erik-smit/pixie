cc9241faf8ff301a3af6f4c039a198bdf26bcc32
erik-smit <smite@zylon.net>
2013-05-12 19:08:15 +0200

    PUIAS support & incomplete attempt at kickstart

---
+++ scripts/puias.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DESTDIR="../pxelinux.cfg"
+DISTROS="6"
+ARCHS="x86_64 i386"
+
+(echo MENU BEGIN PUIAS;
+for DISARCH in $ARCHS; do
+  echo MENU BEGIN $DISARCH
+    for DISTRO in $DISTROS; do
+      BASEURL="http://nl.mirror.eurid.eu/puias/${DISTRO}/${DISARCH}"
+      INSTURL="$BASEURL/os"
+      PXEURL="$BASEURL/os/images/pxeboot"
+      KSURL="http://pixie.zylon.net/kickstart"
+      echo LABEL PUIAS${DISTRO}${DISARCH}
+      echo  MENU LABEL PUIAS ${DISTRO} ${DISARCH} - Interactive
+      echo  KERNEL $PXEURL/vmlinuz
+      echo  INITRD $PXEURL/initrd.img
+      echo LABEL PUIAS${DISTRO}${DISARCH}-ks-md0-sda1-sdb1
+      echo  MENU LABEL PUIAS ${DISTRO} ${DISARCH} - Kickstart /dev/md0 '(/dev/sd[ab]1)'
+      echo  KERNEL $PXEURL/vmlinuz ks=$KSURL/PUIAS/$DISTRO/$DISARCH/md0-sda1-sdb1.cfg ksdevice=eth0
+      echo  INITRD $PXEURL/initrd.img
+      echo LABEL PUIAS${DISTRO}${DISARCH}-ks-sda1
+      echo  MENU LABEL PUIAS ${DISTRO} ${DISARCH} - Kickstart /dev/sda1
+      echo  KERNEL $PXEURL/vmlinuz ks=$KSURL/PUIAS/$DISTRO/$DISARCH/sda1.cfg ksdevice=eth0
+      echo  INITRD $PXEURL/initrd.img
+  done
+  echo MENU END
+done
+echo MENU END
+) > ${DESTDIR}/puias.cfg
+
--- scripts/scientific.sh
+++ scripts/scientific.sh
@@ -8,10 +8,22 @@ ARCHS="x86_64 i386"
 for DISARCH in $ARCHS; do
   echo MENU BEGIN $DISARCH
     for DISTRO in $DISTROS; do
-      URL="http://nl.mirror.eurid.eu/scientific/${DISTRO}/${DISARCH}/os/images/pxeboot"
-      echo LABEL Scientific ${DISTRO} ${DISARCH}
-      echo  KERNEL $URL/vmlinuz
-      echo  INITRD $URL/initrd.img
+      BASEURL="http://nl.mirror.eurid.eu/scientific/${DISTRO}/${DISARCH}"
+      INSTURL="$BASEURL/os"
+      PXEURL="$BASEURL/os/images/pxeboot"
+      KSURL="http://pixie.zylon.net/kickstart"
+      echo LABEL Scientific${DISTRO}${DISARCH}
+      echo  MENU LABEL Scientific ${DISTRO} ${DISARCH} - Interactive
+      echo  KERNEL $PXEURL/vmlinuz
+      echo  INITRD $PXEURL/initrd.img
+      echo LABEL Scientific${DISTRO}${DISARCH}-ks-md0-sda1-sdb1
+      echo  MENU LABEL Scientific ${DISTRO} ${DISARCH} - Kickstart /dev/md0 '(/dev/sd[ab]1)'
+      echo  KERNEL $PXEURL/vmlinuz ks=$KSURL/Scientific/$DISTRO/$DISARCH/md0-sda1-sdb1.cfg ksdevice=eth0
+      echo  INITRD $PXEURL/initrd.img
+      echo LABEL Scientific${DISTRO}${DISARCH}-ks-sda1
+      echo  MENU LABEL Scientific ${DISTRO} ${DISARCH} - Kickstart /dev/sda1
+      echo  KERNEL $PXEURL/vmlinuz ks=$KSURL/Scientific/$DISTRO/$DISARCH/sda1.cfg ksdevice=eth0
+      echo  INITRD $PXEURL/initrd.img
   done
   echo MENU END
 done

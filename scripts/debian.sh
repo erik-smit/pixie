26248994eb8586dc0f625a984e83adc89289dd27
erik-smit <smite@zylon.net>
2013-07-09 13:36:25 +0200

    Autogenerate distrolist

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+BASEURL="http://ftp.nl.debian.org/debian/dists/"
 DESTDIR="../pxelinux.cfg"
-DISTROS="squeeze wheezy sid"
+DISTROS=`lftp $BASEURL -e 'ls;quit' 2> /dev/null | awk '{ print $3; }' | grep -v -- - | grep -v ^$`
 ARCHS="amd64 i386"
 
 (echo MENU BEGIN Debian GNU/Linux;
 for DISARCH in $ARCHS; do
   echo MENU BEGIN $DISARCH
     for DISTRO in $DISTROS; do
-      URL="http://ftp.nl.debian.org/debian/dists/${DISTRO}/main/installer-${DISARCH}/current/images/netboot"
+      URL="${BASEURL}/${DISTRO}/main/installer-${DISARCH}/current/images/netboot"
+      CONFIGURL="${URL}/pxelinux.cfg/default"
+
+      if curl -I -s ${CONFIGURL} | head -n1 | grep -qv "HTTP/1.1 200"; then
+        continue
+      fi
+
       echo LABEL Debian ${DISTRO} ${DISARCH}
-echo  CONFIG ${URL}/debian-installer/${DISARCH}/pxelinux.cfg/default
-echo  APPEND ${URL}/
+      echo  CONFIG ${CONFIGURL}
+      echo  APPEND ${URL}/
   done
   echo MENU END
 done
 echo MENU END
 ) > ${DESTDIR}/debian.cfg
-

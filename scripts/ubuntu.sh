791274272723a698bd3db1b2a49c3f5e679f0fa6
erik-smit <smite@zylon.net>
2013-07-09 13:41:16 +0200

    Autogenerate distrolist for ubuntu

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+BASEURL="http://nl.archive.ubuntu.com/ubuntu/dists/"
 DESTDIR="../pxelinux.cfg"
-DISTROS="quantal precise oneiric natty maverick lucid hardy"
+DISTROS=`lftp $BASEURL -e 'ls;quit' 2> /dev/null | awk '{ print $5; }' | grep -v -- - | grep -v ^$`
 ARCHS="amd64 i386"
 
-(echo MENU BEGIN Ubuntu Linux;
+(echo MENU BEGIN Ubuntu GNU/Linux;
 for DISARCH in $ARCHS; do
   echo MENU BEGIN $DISARCH
     for DISTRO in $DISTROS; do
-      URL="http://nl.archive.ubuntu.com/ubuntu/dists/${DISTRO}/main/installer-${DISARCH}/current/images/netboot/"
-      echo LABEL Ubuntu ${DISTRO} 
-echo  CONFIG ${URL}pxelinux.cfg/default
-echo  APPEND ${URL}
+      URL="${BASEURL}/${DISTRO}/main/installer-${DISARCH}/current/images/netboot"
+      CONFIGURL="${URL}/pxelinux.cfg/default"
+
+      if curl -I -s ${CONFIGURL} | head -n1 | grep -qv "HTTP/1.1 200"; then
+        continue
+      fi
+
+      echo LABEL Ubuntu ${DISTRO} ${DISARCH}
+      echo  CONFIG ${CONFIGURL}
+      echo  APPEND ${URL}/
   done
   echo MENU END
 done
 echo MENU END
 ) > ${DESTDIR}/ubuntu.cfg
-

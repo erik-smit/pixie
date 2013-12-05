107032822d3c48c034d88a63a39e9ca60a1ea50d
erik-smit <smite@zylon.net>
2013-12-05 17:58:38 +0100

    Ubuntu doesn't update releases, but instead places updated variants on new locations. Search for latest netboot and take it.

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -2,20 +2,24 @@
 
 BASEURL="http://nl.archive.ubuntu.com/ubuntu/dists/"
 DESTDIR="../pxelinux.cfg"
-DISTROS=`lftp $BASEURL -e 'ls;quit' 2> /dev/null | awk '{ print $5; }' | grep -v -- - | grep -v ^$`
+DISTROS=`lftp $BASEURL -e 'ls;quit' 2> /dev/null | awk '{ print $5; }' | grep -v ^$`
 ARCHS="amd64 i386"
 
 (echo MENU TITLE Ubuntu Linux;
 for DISARCH in $ARCHS; do
   echo MENU BEGIN $DISARCH
     for DISTRO in $DISTROS; do
-      URL="${BASEURL}/${DISTRO}/main/installer-${DISARCH}/current/images/netboot"
+      URL="${BASEURL}/${DISTRO}/main/installer-${DISARCH}/current/images"
       CONFIGURL="${URL}/pxelinux.cfg/default"
 
-      if curl -I -s ${CONFIGURL} | head -n1 | grep -qv "HTTP/1.1 200"; then
+      # Ubuntu provides alternate netboot packages, based on kernel of later Ubuntu release, for hardware-support. Take latest.
+      NETBOOTVARIANT=`lftp $URL -e 'ls;quit' 2> /dev/null | awk '{ print $5; }' | grep netboot | tail -n1`
+
+      if ! `echo $NETBOOTVARIANT | grep -q netboot`; then
         continue
       fi
 
+      URL="${URL}/${NETBOOTVARIANT}"
       echo LABEL Ubuntu ${DISTRO} ${DISARCH}
       echo  CONFIG ${CONFIGURL}
       echo  APPEND ${URL}/

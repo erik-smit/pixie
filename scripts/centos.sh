1026f1b76e47a31d59851196400288491ea6fa7b
erik-smit <smite@zylon.net>
2014-07-17 14:12:33 +0200

    centos 7

--- scripts/centos.sh
+++ scripts/centos.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 DESTDIR="../pxelinux.cfg"
-DISTROS="7 6 5"
+DISTROS="6 5"
 ARCHS="x86_64 i386"
 
-(echo MENU TITLE CentOS;
+(
+echo MENU TITLE CentOS
+
+BASEURL="http://ftp.tudelft.nl/centos.org/7/os/x86_64"
+PXEURL="${BASEURL}/images/pxeboot"
+echo LABEL CentOS 7 x86_64
+echo  KERNEL $PXEURL/vmlinuz
+echo  INITRD $PXEURL/initrd.img
+echo  APPEND inst.repo=$BASEURL
+
 for DISARCH in $ARCHS; do
-  echo MENU BEGIN $DISARCH
-    for DISTRO in $DISTROS; do
-      URL="http://ftp.tudelft.nl/centos.org/${DISTRO}/os/${DISARCH}/images/pxeboot"
-      if ! wget --quiet --output-document=/dev/null $URL 2> /dev/null; then 
-        continue; fi
-      echo LABEL CentOS ${DISTRO} ${DISARCH}
-      echo  KERNEL $URL/vmlinuz
-      echo  INITRD $URL/initrd.img
+  for DISTRO in $DISTROS; do
+    BASEURL="http://ftp.tudelft.nl/centos.org/${DISTRO}/os/${DISARCH}"
+    PXEURL="${BASEURL}/images/pxeboot"
+    echo LABEL CentOS ${DISTRO} ${DISARCH}
+    echo  KERNEL $URL/vmlinuz
+    echo  INITRD $URL/initrd.img
+    echo  APPEND repo=$BASEURL
   done
-  echo MENU END
 done
 ) > ${DESTDIR}/centos.cfg
 
